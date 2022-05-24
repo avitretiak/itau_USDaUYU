08f31ea5b248e1bc82308c183ce60cf9cc091a86
Avi Tretiak <avitretiak@gmail.com>
2022-05-24 01:15:56 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 // ==UserScript==
 // @name         Itau dolares a pesos
 // @version      1
-// @description  Convertir saldito en USD a UYU en itau y mostrar lindo
-// @author       Abi
+// @description  Convertir saldo de USD a UYU y mostrar
+// @author       Avi
 // @match        https://www.itaulink.com.uy/*
 // @grant        none
 // ==/UserScript==
 
 function mostrarPesos() {
-    let selectorCuentas = document.querySelectorAll('.zebra-row');
-    let cuentaDolares = selectorCuentas[0];
+    let cuentaDolares = document.querySelectorAll('.zebra-row')[0];
     
     let columnaSaldo = cuentaDolares.querySelectorAll('.col-sm-4')[2];
     let selectorSaldoDolares = columnaSaldo.querySelector('.saldo-valor');
